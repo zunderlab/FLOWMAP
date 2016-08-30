5d2f132dc4348fa262dd8c4ea84db880d865d2ba
Melissa Ko <mesako@stanford.edu>
2016-08-29 22:11:52 -0700

    added function/variable annotations for new users

--- FLOWMAP_run.R
+++ FLOWMAP_run.R
@@ -1,92 +1,90 @@
 rm(list=ls())
 
+# Copyright statement comment
+# Author comment
+# File description comment,
+# including purpose of program,
+# inputs, and outputs
+
+# source() and library() statements
+# Function definitions
+# Executed statements, if applicable (e.g., print, plot)
+
 prefolder <- "/Users/mesako/Desktop/Work/Research/Code/FLOW-MAP/"
-# prefolder <- "C:\\Users\\Eli\\Documents\\Lab\\Manuscripts\\2016 - CPiSCB - FLOW-MAP\\Code\\FLOW-MAP\\"
+# prefolder specifies the folder where all FLOW-MAP
+# code is saved on your local computer
+
+# uses prefolder to load in all FLOW-MAP files/functions
 source(paste(prefolder, "FLOWMAP_main.R", sep = ""))
 
-# MULTI_FOLDER <- "~/Desktop/Work/Research/Raw FCS Files/20150804TRAILHeLa"
-# listOfTreatments <- c("DMSO", "MEKi", "p38i")
-# FOLDER <- "~/Desktop/Work/Research/Raw FCS Files/20150728TRAILHeLa"
-# FOLDER <- "/Users/mesako/Desktop/Work/Research/Code/FLOW-MAP_20160824/Synthetic Data"
-FOLDER <- "/Users/mesako/Desktop/Work/Research/Raw FCS Files/20160721_MM1S_Timecourse3_Live/WT/J"
-# FOLDER <- "/Users/mesako/Desktop/Work/Research/Raw FCS Files/20160721_MM1S_Timecourse3_Live/WT/BO"
-# FOLDER <- "C:\\Users\\Eli\\Documents\\Lab\\Manuscripts\\2016 - CPiSCB - FLOW-MAP\\Code\\FLOW-MAP\\Synthetic Data\\"
-FILE_FORMAT <- "*.fcs"
-VAR_ANNOTATE <- list("In113Di"	= "activeBak", "La139Di"	= "cPARP",
-                     "Ce140Di"	= "Bak", "Pr141Di"	= "pp38", "Nd146Di"	= "pBcl2",
-                     "Nd148Di"	= "pErk", "Sm149Di"	= "APAF", "Nd150Di"	= "pRb",
-                     "Sm152Di"	= "pAkt", "Eu153Di"	= "BclxL", "Sm154Di"	= "Bax",
-                     "Gd155Di"	= "activeBak", "Gd156Di"	= "CyclinB", "Gd157Di"	= "Bcl2",
-                     "Gd158Di"	= "pSTAT5", "Gd160Di"	= "Mcl1", "Dy161Di"	= "cMyc",
-                     "Dy164Di"	= "IkBalpha", "Ho165Di"	= "Bim", "Er167Di"	= "CyclinA",
-                     "Er168Di"	= "pH3", "Er170Di"	= "pBadS112", "Yb171Di"	= "pZAP70",
-                     "Yb172Di"	= "Bclw", "Yb173Di"	= "cCaspase3", "Yb174Di"	= "p53",
-                     "Lu175Di"	= "pS6", "Lu176Di" = "pCREB", "Pt195Di" = "Cisplatin")
-VAR_REMOVE <- c("Time", "Event_length", "beadDist", "Bi209Di",
-                "Pt194Di", "Ir193Di", "Ir191Di", "Tm169Di",
-                "Er166Di", "Dy162Di", "Dy163Di", "Tb159Di",
-                "Y89Di", "Pd102Di", "Pd104Di", "Pd105Di", "Pd106Di",
-                "Pd108Di", "Pd110Di", "In115Di", "I127Di",
-                "Nd142Di", "Nd143Di", "Nd144Di", "Nd145Di",
-                "Pm147Di", "Eu151Di", "FileNum")
-# VAR_ANNOTATE <- list("Marker1" = "Marker1", "Marker2" = "Marker2")
-# VAR_REMOVE <- c()
+folder <- "/Users/mesako/Desktop/Work/Research/Code/FLOW-MAP_20160824/Synthetic Data"
+# folder specifies the folder where the FCS files
+# to be analyzed are saved on your local computer
+
+file.format <- "*.fcs"
+# file.format specifies what kind of files to look
+# for in "folder," all functions currently only work
+# with FCS files
+
+var.annotate <- list("Marker1" = "Marker1", "Marker2" = "Marker2")
+# var.annotate specifies what names each parameter
+# from the FCS file should be called, for example,
+# converting metal/fluorescence channels (e.g. "FITC")
+# and renaming them according to marker (e.g. "CD44")
+
+var.remove <- c()
+# var.remove specifies any parameters from the FCS files
+# that should be entirely excluded from downstream
+# analysis, specify based on post-annotation name
 
 per <- 1
+# per specifies n for top n percent of edges used
+# between all nodes in graph under consideration for
+# forming edges between
+
 minimum <- 2
+# minimum specifies the minimum number of edges any
+# given node in graph will have
+
 maximum <- 3
-distance_metric <- "manhattan" # or "euclidean"
-# small
-# SUBSAMPLE <- 5000
-# CLUSTNUM <- 500
-# large
-SUBSAMPLE <- 20000
-CLUSTNUM <- 1000
-seedX <- 3
-set.seed(seedX)
+# maximum specifies the maximum number of edges any
+# given node in graph will have
 
-# CLUSTERING_VAR <- c("activeBak", "cPARP", "Bak", "pBcl2",
-#                     "APAF","BclxL", "Bax", "activeBak", "Bcl2",
-#                     "Mcl1", "Bim", "pBadS112", "Bclw", "cCaspase3")
-# CLUSTERING_VAR <- c("activeBak", "cPARP", "Bak", "pp38", "pBcl2",
-#                     "pErk", "APAF", "pAkt", "BclxL", "Bax",
-#                     "activeBak", "Bcl2", "pSTAT5", "Mcl1",
-#                     "IkBalpha", "Bim", "pBadS112", "pZAP70",
-#                     "Bclw", "cCaspase3", "pS6", "pCREB")
-CLUSTERING_VAR <- c("activeBak", "cPARP", "Bak", "pp38", "pBcl2",
-                    "pErk", "APAF", "pRb", "pAkt", "BclxL", "Bax",
-                    "activeBak", "CyclinB", "Bcl2", "pSTAT5",
-                    "Mcl1", "cMyc", "IkBalpha", "Bim", "CyclinA",
-                    "pH3", "pBadS112", "pZAP70", "Bclw", "cCaspase3",
-                    "p53", "pS6", "pCREB")
-# CLUSTERING_VAR <- c("marker1", "marker2")
-setwd(FOLDER)
+distance.metric <- "manhattan" # other option is "euclidean"
+# distance.metric specifies how the distance/weight
+# between nodes will be calculated, in order to determine
+# which edges are assigned and what is their weight
 
-singleFLOWMAP(FOLDER, FILE_FORMAT, VAR_REMOVE, VAR_ANNOTATE,
-              CLUSTERING_VAR, CLUSTNUM, SUBSAMPLE, distance_metric,
+subsample <- 5000
+# subsample specifies how many measurements/events/cells
+# to take from each FCS file, each file must contain at
+# least this many events for analysis to proceed
+
+cluster.number <- 500
+# cluster.number specifies how many clusters to identify
+# for the subsampled events from each separate FCS file
+
+seed.X <- 3
+set.seed(seed.X)
+# seed.X specifies the seed for a given run, this should
+# lead to reproducible runs of FLOW-MAP and its resulting
+# figures for the same seed
+
+clustering.var <- c("marker1", "marker2")
+# clustering.var specifies which parameters to use for
+# calculating distance between nodes, all markers not
+# listed will not be considered and will have no influence
+# on the shape of the resulting FLOW-MAP, but will still
+# be seen as a parameter in the final PDFs
+
+setwd(folder)
+singleFLOWMAP(folder, file.format, var.remove, var.annotate,
+              clustering.var, cluster.number, subsample, distance.metric,
               minimum, maximum, per, shuffle = TRUE)
-
-
-# fcs_file_names <- getFCSNames(FOLDER, FILE_FORMAT)
-# output_folder <- makeOutFolder(runtype = "singleFLOWMAP")
-# setwd(output_folder)
-# fcs_files <- loadCleanFCS(fcs_file_names, VAR_REMOVE, VAR_ANNOTATE, subsample = SUBSAMPLE, subsampleRand = TRUE)
-# for (i in 1:length(fcs_files)) {
-#   df1 <- fcs_files[[i]]
-#   df2 <- df1[sample(nrow(df1)), ]
-#   fcs_files[[i]] <- df2
-# }
-# file_clusters <- clusterFCS(fcs_files = fcs_files, channel_cluster = CLUSTERING_VAR, numcluster = CLUSTNUM,
-#                             distance_metric = distance_metric)
-# graph <- buildFLOWMAP(file_clusters, per = per, min = minimum,
-#                       max = maximum, distance_metric, cellnum = SUBSAMPLE)
-# file_name <- paste(basename(FOLDER), "original_edge_choice", sep = "_")
-# convertToGraphML(graph, file_name)
-# graph_xy <- forceDirectedXY(graph)
-# file_name_xy <- paste(basename(FOLDER), "original_edge_choice", "xy", sep = "_")
-# final_file_name <- convertToGraphML(graph_xy, file_name_xy)
-# convertToPDF(final_file_name, edge_color = "#FF000000")
-# printSummary()
+# singleFLOWMAP function, with correctly provided folders
+# and variables above, should run from start to finish,
+# producing PDFs and graphml files in a new subfolder within
+# the "folder" that contains the FCS files
 
 
 # multiFLOWMAP(listOfTreatments, MULTI_FOLDER, FILE_FORMAT, VAR_REMOVE, VAR_ANNOTATE,
