54ee0e88d40c1ce07a9cd59e7b5a08edd2f587ae
Rohit Rustagi <rsr8gx@virginia.edu>
2017-06-23 12:39:13 -0400

    Corrupt GUI fix

--- R/launch-GUI.R
+++ R/launch-GUI.R
@@ -352,8 +352,8 @@ LaunchGUI <- function() {
                         inputs[["resultDir"]])
   }
   if(quit.var == FALSE){
-    runApp(appDir = file.path(system.file(package = "FLOWMAPR"), "shinyGUI"))
-    # runApp(appDir = "C:\\Users\\Rohit\\Desktop\\FLOWMAP\\inst\\shinyGUI")
+    runApp(appDir = file.path(system.file(package = "FLOWMAPR"), "application"))
+    # runApp(appDir = "C:/Users/Rohit/Desktop/Application")
   } else {
     stop("Analysis has be cancelled")
   }
