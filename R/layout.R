9b7bef6e70f56857f49b231b976db1080a8e71b9
smgoggin10 <sarahmgoggin@gmail.com>
2020-07-24 15:17:44 -0400

    stop using scaffold for forceatlas2

--- R/layout.R
+++ R/layout.R
@@ -1,19 +1,19 @@
 
-ForceDirectedXY <- function(graph) {
-  V(graph)$size <- rep(20, vcount(graph))
-  force.graph1 <- scaffold:::layout.forceatlas2(graph, iter = 10000,
-                                                stopping_tolerance = 0.001,
-                                                prevent.overlap = FALSE)
-  graph.with.xy <- graph
-  V(graph.with.xy)$x <- force.graph1$lay[, 1]
-  V(graph.with.xy)$y <- force.graph1$lay[, 2]
-  force.graph2 <- scaffold:::layout.forceatlas2(graph.with.xy, iter = 1000,
-                                                stopping_tolerance = 0.001,
-                                                prevent.overlap = TRUE)
-  V(graph.with.xy)$x <- force.graph2$lay[, 1]
-  V(graph.with.xy)$y <- force.graph2$lay[, 2]
-  return(graph.with.xy)
-}
+# ForceDirectedXY <- function(graph) {
+#   V(graph)$size <- rep(20, vcount(graph))
+#   force.graph1 <- scaffold::layout.forceatlas2(graph, iter = 10000,
+#                                                 stopping_tolerance = 0.001,
+#                                                 prevent.overlap = FALSE)
+#   graph.with.xy <- graph
+#   V(graph.with.xy)$x <- force.graph1$lay[, 1]
+#   V(graph.with.xy)$y <- force.graph1$lay[, 2]
+#   force.graph2 <- scaffold::layout.forceatlas2(graph.with.xy, iter = 1000,
+#                                                 stopping_tolerance = 0.001,
+#                                                 prevent.overlap = TRUE)
+#   V(graph.with.xy)$x <- force.graph2$lay[, 1]
+#   V(graph.with.xy)$y <- force.graph2$lay[, 2]
+#   return(graph.with.xy)
+# }
 
 RunForceDirectedLayout <- function(mode, file.name, graph, orig.times=NULL, which.palette=NULL) {
   global.graph.pre.ml <<- graph
