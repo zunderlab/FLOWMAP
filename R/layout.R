56d90b1525c1d205b9fdf733c4d8a0656496d4f7
Sarah Goggin <35312830+smgoggin10@users.noreply.github.com>
2020-07-26 15:55:08 -0400

    fix forcelayout2

--- R/layout.R
+++ R/layout.R
@@ -1,13 +1,13 @@
 
 ForceDirectedXY <- function(graph) {
   V(graph)$size <- rep(20, vcount(graph))
-  force.graph1 <- scaffold:::layout.forceatlas2(graph, iter = 10000,
+  force.graph1 <- scaffold::layout.forceatlas2(graph, iter = 10000,
                                                 stopping_tolerance = 0.001,
                                                 prevent.overlap = FALSE)
   graph.with.xy <- graph
   V(graph.with.xy)$x <- force.graph1$lay[, 1]
   V(graph.with.xy)$y <- force.graph1$lay[, 2]
-  force.graph2 <- scaffold:::layout.forceatlas2(graph.with.xy, iter = 1000,
+  force.graph2 <- scaffold::layout.forceatlas2(graph.with.xy, iter = 1000,
                                                 stopping_tolerance = 0.001,
                                                 prevent.overlap = TRUE)
   V(graph.with.xy)$x <- force.graph2$lay[, 1]
